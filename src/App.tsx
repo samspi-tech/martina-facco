3aed46c10c2c4bd0f8f8ae603183b320ea99ac59
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-12-28 18:22:07 +0100

    feat: add React Router for page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import Homepage from '@/pages/homepage/Homepage.tsx';
+import ArtDirection from '@/pages/artDirection/ArtDirection.tsx';
+import Campaigns from '@/pages/campaigns/Campaigns.tsx';
+import GraphicDesign from '@/pages/graphicDesign/GraphicDesign.tsx';
+import ContactMe from '@/pages/contactMe/ContactMe.tsx';
+
 const App = () => {
-    return <div>Hello, World!</div>;
+    return (
+        <Router>
+            <Routes>
+                <Route index element={<Homepage />} />
+                <Route path="/art-direction" element={<ArtDirection />} />
+                <Route path="/campaigns" element={<Campaigns />} />
+                <Route path="/graphic-design" element={<GraphicDesign />} />
+                <Route path="/contact-me" element={<ContactMe />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
