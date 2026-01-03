c657fccd5248ba449d08dd0443b9d0402d2be709
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-01-03 05:45:34 +0100

    refactor(footer): socials list

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,28 +1,30 @@
 import styles from './Footer.module.css';
-import { socialIcons } from '@/components/footer/dataSource.ts';
+import { socialsList } from '@/components/footer/dataSource.ts';
 
 const Footer = () => {
     const currYear = new Date().getFullYear();
 
     return (
         <footer className={styles.footer}>
-            <div className={styles.iconsContainer}>
-                {socialIcons.map((socialIcon) => {
-                    const { id, icon, name, link } = socialIcon;
+            <ul>
+                {socialsList.map((social) => {
+                    const { id, icon, name, link } = social;
                     const Icon = icon;
 
                     return (
-                        <a
-                            key={id}
-                            href={link}
-                            target="_blank"
-                            aria-label={name}
-                        >
-                            <Icon />
-                        </a>
+                        <li>
+                            <a
+                                key={id}
+                                href={link}
+                                target="_blank"
+                                aria-label={name}
+                            >
+                                <Icon />
+                            </a>
+                        </li>
                     );
                 })}
-            </div>
+            </ul>
             <p>Copyright &copy; {currYear} &ndash; Martina Facco</p>
         </footer>
     );
--- src/components/footer/dataSource.ts
+++ src/components/footer/dataSource.ts
@@ -5,27 +5,27 @@ import {
     FaPinterestSquare,
 } from 'react-icons/fa';
 
-export const socialIcons = [
+export const socialsList = [
     {
-        id: 'social-icon-1',
+        id: 'social-1',
         icon: FaInstagramSquare,
         name: 'Instagram',
         link: 'https://www.instagram.com/martinafacco_/',
     },
     {
-        id: 'social-icon-2',
+        id: 'social-2',
         icon: FaLinkedin,
         name: 'LinkedIn',
         link: 'https://www.linkedin.com/in/martinafacco/',
     },
     {
-        id: 'social-icon-3',
+        id: 'social-3',
         icon: FaBehanceSquare,
         name: 'Behance',
         link: 'https://www.behance.net/martinafacco3',
     },
     {
-        id: 'social-icon-4',
+        id: 'social-4',
         icon: FaPinterestSquare,
         name: 'Pinterest',
         link: 'https://it.pinterest.com/rosemarino/',
