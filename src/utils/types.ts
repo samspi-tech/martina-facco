5193d6663f2481692f4ea3b065984eeb1a206604
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-01-07 22:04:01 +0100

    chore: add galleries types

---
+++ src/utils/types.ts
@@ -0,0 +1,23 @@
+type media = {
+    id: string;
+    fileName: string;
+};
+
+export type Gallery = {
+    id: string;
+    title: string;
+    projectScope?: string[];
+    agency?: string;
+    isFreelance?: boolean;
+    isPersonalProject?: boolean;
+    year?: string;
+    galleryName: string;
+    images?: media[];
+    videos?: media[];
+};
+
+export type Galleries = {
+    artDirection: Gallery[];
+    campaigns: Gallery[];
+    graphicDesign: Gallery[];
+};
