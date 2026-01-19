8bc060865b20b1ac1d0638108ffeda745ee37d77
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-01-19 20:21:23 +0100

    perf(use-galleries)!: remove caching in session storage

--- src/hooks/useGalleries.ts
+++ src/hooks/useGalleries.ts
@@ -3,27 +3,17 @@ import type { Galleries } from '@/utils/types.ts';
 
 export const useGalleries = () => {
     const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState('');
+    const [error, setError] = useState<string | null>('');
     const [galleries, setGalleries] = useState<Galleries | null>(null);
 
     useEffect(() => {
         const getAllGalleries = async () => {
-            const cachedGalleries = sessionStorage.getItem('galleries');
-
-            if (cachedGalleries) {
-                const galleries = JSON.parse(cachedGalleries);
-
-                setGalleries(galleries);
-                return;
-            }
-
             setIsLoading(true);
             try {
-                const res = await fetch('galleries.json');
+                const res = await fetch('/galleries.json');
                 const data: Galleries = await res.json();
 
                 setGalleries(data);
-                sessionStorage.setItem('galleries', JSON.stringify(data));
             } catch (err) {
                 setError('Failed to load galleries');
 
